f1d09cb0805252876c06a9743a846c29386b2733
Solomon Kashani <shlomokashani@gmail.com>
2017-03-20 17:07:09 +0200

    export PATH=/root/inst/bin/:$PATH

--- nice-docker/run_jupyter.sh
+++ nice-docker/run_jupyter.sh
@@ -19,12 +19,18 @@ ls -la
 
 
 #export PATH=$PATH:/root/cling/bin
-export PATH=/root/inst/share/:$PATH
+export PATH=/root/inst/bin/:$PATH
+
+echo $PATH
 
 which cling
 
 NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
 echo "Detected $NCPUS cpus"
+
+python -c "import sys; sys.path.append('/root/inst/bin/')"
+
+export PATH=/root/inst/share/:$PATH
 
 dask-scheduler --host localhost &
 dask-worker localhost:8786 $* &
