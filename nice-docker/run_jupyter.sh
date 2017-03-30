b5dced4c96d0cc821186bb876302fd45324e71d5
Solomon K <shlomokashani@gmail.com>
2017-03-30 10:05:14 +0300

    Update run_jupyter.sh

--- nice-docker/run_jupyter.sh
+++ nice-docker/run_jupyter.sh
@@ -22,12 +22,15 @@ ls -la
 NCPUS=`python -c "import multiprocessing as mp; print(mp.cpu_count())"`
 echo "Detected $NCPUS cpus"
 
-python -c "import sys; sys.path.append('/root/inst/bin/')"
+#python -c "import sys; sys.path.append('/root/inst/bin/')"
 export PATH=/root/inst/bin/:$PATH
 
 echo $PATH
-which cling
+
 
 #dask-scheduler --host localhost &
 #dask-worker localhost:8786 $* &
 jupyter notebook "$@" &
+
+# run postgress
+su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" &
