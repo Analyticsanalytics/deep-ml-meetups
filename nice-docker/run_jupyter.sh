853c2f2fbee46b3b08ff53f52305af317388fa3d
Solomon K <shlomokashani@gmail.com>
2017-04-06 09:46:15 +0300

    --allow-root

--- nice-docker/run_jupyter.sh
+++ nice-docker/run_jupyter.sh
@@ -30,7 +30,7 @@ echo $PATH
 
 #dask-scheduler --host localhost &
 #dask-worker localhost:8786 $* &
-jupyter notebook "$@" &
+jupyter notebook --allow-root "$@" &
 
 # run postgress
 su postgres -c "/usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" &
